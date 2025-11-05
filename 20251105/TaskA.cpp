f9fad513604ea4a371f4663897b16db1071f3574
MDAIKI1230 <shimamizu1230@gmail.com>
2025-11-05 15:46:55 +0900

    TaskA完成

---
+++ 20251105/TaskA.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    for(int i = 1; i <= 10 ; ++i)
+    {
+        std::cout << i ;
+        std::cout << "\n";
+    }
+
+    std::cout << std::endl;
+
+    return 0;
+}
